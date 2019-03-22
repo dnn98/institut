0105743f088d0bbd12037a609f54867943dd592b
peremoya2000 <perem666@gmail.com>
2019-03-22 16:12:46 +0100

    Escriure Pere a nes main

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,10 +1,10 @@
 package principal;
 
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main{
 	public static void main(String[] args) {
+		System.out.println("Pere Moyà Serra");
 		Menu a = new Menu();
 		a.inici();
 	}
